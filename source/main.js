cca16f22a75f25871c4319a0ee29db31b7bb0fc4
ssgta <62765221+ssgta@users.noreply.github.com>
2023-10-10 18:35:18 +0100

    Add files via upload

---
+++ source/main.js
@@ -0,0 +1,7 @@
+import '../styles/modern-normalise.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/components/about.css';
+import '../styles/components/featured.css';
+import '../styles/utilityclasses.css';
